531c0674aec83b17acd662681b92b7a56bcb3506
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-06-24 12:01:35 +0200

    Heading

---
+++ Heading.pas
@@ -0,0 +1,11 @@
+PROGRAM headings(input,input);
+VAR a,b,total:INTEGER;
+BEGIN
+     a:=4;
+     b:=5;
+     WRITELN('        A         B');
+     WRITELN(a:12,b:12);
+     WRITELN;
+     total:=a+b;
+     WRITELN('A+B =',total:5)     
+END.
