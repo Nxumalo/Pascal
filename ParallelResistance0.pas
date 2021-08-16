021ae5b46cd65a190f8eb1ccbdf316dc3ba2311c
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-08-16 18:21:19 +0200

    Parallel Resistance with Syntax Error

---
+++ ParallelResistance0.pas
@@ -0,0 +1,14 @@
+PROGRAM ParallelResistance0(input,output); (*$B-*)
+
+(* Calculation of the total resistance of two resistance in parallel *)
+
+VAR pr,r1,r2,recip:REAL;
+BEGIN
+        READ(r1,r2):
+        
+        recip :=1/r1+r2;
+        pr := 1/recip;
+        WRITELN('       R1      R2      TOTAL PARALLEL RESISTANCE');
+        WRITLEN(r1:6:2,r2:6:2,pr:16:2);
+        
+END.        
