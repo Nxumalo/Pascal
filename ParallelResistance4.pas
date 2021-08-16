5d472c02566708328c2a8764fb43e29e93155da8
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-08-16 19:00:50 +0200

    Parallel Resistance  with Calculation Errors

---
+++ ParallelResistance4.pas
@@ -0,0 +1,23 @@
+PROGRAM ParallelResistance4(input,output); (*$B-*)
+
+(* Calculation of the total reistance of two resistance in parallel *)
+
+VAR pr,r1,r2,recip: REAL;
+
+BEGIN
+
+        READ(r1,r2);
+        
+        WRITELN('R1=',r1,6:2, 'R2=', r2:6:2);        (*  DEBUG WRITE *)
+        
+        recip:=1/(r1+r2);
+        
+        WRITELN('RECIP=',recip:10:4);                (*  DEBUG WRITE *)
+        
+        pr:1/recip;
+        
+        WRITELN('     R1      R2      TOTAL PARALLEL RESISTANCE');
+        
+        WRITELN(r1:6:2,r2:6:2,pr:16:2);
+               
+END.        
