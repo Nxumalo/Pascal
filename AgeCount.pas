534fcd6346bdd6459ea3ca8821f044617b659e55
Fraser Nxumalo <3538264@myuwc.ac.za>
2021-08-20 19:38:17 +0200

    IF ... THEN ... Statements

---
+++ AgeCount.pas
@@ -0,0 +1,17 @@
+PROGRAM AgeCount(input,output); (*$B-*);
+
+(* This program illustrates IF ... THEN ... statements *)
+
+(* This program reads in any number of peoples' ages and 
+  calculates:
+    (a) The number of people over 65
+    (b) The number of people over 60. and
+    (c) The average age of the people over 60.
+  finally each of these values is printed.
+  
+  The program is terminated by reading a negative number *)
+  
+VAR Over65Total, Over65Total : INTEGER;
+      
+    
+    
